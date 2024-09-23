863806cc2b8e701f220fa1d960191c876a83eacf
prowider5565 <prowider.dev@gmail.com>
2024-09-23 19:06:47 +0500

    Remove sensitive data

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import openai
 
 # Set up your OpenAI API key
-openai.api_key = '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'
+openai.api_key = ''
 
 # Define a function to get a response from GPT-3.5
 def get_gpt_response(prompt):
